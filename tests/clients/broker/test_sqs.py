f3f60e52e2a240219374c722b222233a1889526a
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-11-04 16:09:52 +0200

    cover all parts of the sqs clients with tests

--- tests/clients/broker/test_sqs.py
+++ tests/clients/broker/test_sqs.py
@@ -18,6 +18,30 @@ if TYPE_CHECKING:
     from pytest_mock import MockerFixture
 
 
+@pytest.fixture
+def sample_broker_message() -> BrokerMessage:
+    return BrokerMessage(metadata={"key": "value"}, body="body")
+
+
+@pytest.fixture
+def sample_client_error() -> ClientError:
+    return ClientError(
+        {
+            "Error": {
+                "Code": "NoSuchBucket",
+                "Message": "The specified bucket does not exist",
+                "BucketName": "sample-bucket",
+            }
+        },
+        "ListBuckets",
+    )
+
+
+@pytest.fixture
+def sample_boto_core_error() -> BotoCoreError:
+    return BotoCoreError()
+
+
 class SampleSQSMessageBuilder(SQSMessageBuilder):
     def build_metadata(self, *args: Any, **kwargs: Any) -> dict[str, Any] | None:
         return {"key": "value"}
@@ -69,61 +93,53 @@ class TestSQSMessageBuilder:
 
 
 class TestSQSClientBase:
-    def test_log_client_error(self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture) -> None:
+    def test_log_client_error(
+        self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture, sample_client_error: ClientError
+    ) -> None:
         spy_logger_error = mocker.spy(broker_clients_logger, "error")
 
-        error_response = {
-            "Error": {
-                "Code": "NoSuchBucket",
-                "Message": "The specified bucket does not exist",
-                "BucketName": "sample-bucket",
-            }
-        }
-        operation_name = "ListBuckets"
-        client_error = ClientError(error_response, operation_name)
-
         with caplog.at_level(logging.ERROR):
-            SQSClientBase().log_client_error(client_error)
+            SQSClientBase().log_client_error(sample_client_error)
 
         assert len(caplog.records) == 1
         log_record = caplog.records[0]
 
         assert "ClientError" in log_record.message
-        assert error_response["Error"]["Code"] in log_record.message
-        assert error_response["Error"]["Message"] in log_record.message
+        assert sample_client_error.response["Error"]["Code"] in log_record.message
+        assert sample_client_error.response["Error"]["Message"] in log_record.message
 
         spy_logger_error.assert_called_once_with(
             log_record.message,
-            exc_info=client_error,
+            exc_info=sample_client_error,
             extra={
                 "error": {
-                    "code": error_response["Error"]["Code"],
-                    "message": error_response["Error"]["Message"],
+                    "code": sample_client_error.response["Error"]["Code"],
+                    "message": sample_client_error.response["Error"]["Message"],
                 }
             },
         )
 
-    def test_log_boto_core_error(self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture) -> None:
+    def test_log_boto_core_error(
+        self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture, sample_boto_core_error: BotoCoreError
+    ) -> None:
         spy_logger_error = mocker.spy(broker_clients_logger, "error")
 
-        boto_core_error = BotoCoreError()
-
         with caplog.at_level(logging.ERROR):
-            SQSClientBase().log_boto_core_error(boto_core_error)
+            SQSClientBase().log_boto_core_error(sample_boto_core_error)
 
         assert len(caplog.records) == 1
         log_record = caplog.records[0]
 
         assert "BotoCoreError" in log_record.message
-        spy_logger_error.assert_called_once_with(log_record.message, exc_info=boto_core_error)
+        spy_logger_error.assert_called_once_with(log_record.message, exc_info=sample_boto_core_error)
 
-    def test_log_success_without_extra(self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture) -> None:
+    def test_log_success_without_extra(
+        self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture, sample_broker_message: BrokerMessage
+    ) -> None:
         spy_info = mocker.spy(broker_clients_logger, "info")
 
-        message = BrokerMessage(metadata={"key": "value"}, body="body")
-
         with caplog.at_level(logging.INFO):
-            SQSClientBase().log_success(message)
+            SQSClientBase().log_success(sample_broker_message)
 
         assert len(caplog.records) == 1
         log_record = caplog.records[0]
@@ -131,13 +147,13 @@ class TestSQSClientBase:
         assert "success" in log_record.message
         spy_info.assert_called_once_with(caplog.records[0].message, extra={})
 
-    def test_log_success_with_extra(self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture) -> None:
+    def test_log_success_with_extra(
+        self, mocker: MockerFixture, caplog: pytest.LogCaptureFixture, sample_broker_message: BrokerMessage
+    ) -> None:
         spy_info = mocker.spy(broker_clients_logger, "info")
 
-        message = BrokerMessage(metadata={"key": "value"}, body="body")
-
         with caplog.at_level(logging.INFO):
-            SQSClientBase(log_attributes=True, log_body=True).log_success(message)
+            SQSClientBase(log_attributes=True, log_body=True).log_success(sample_broker_message)
 
         assert len(caplog.records) == 1
         log_record = caplog.records[0]
@@ -146,8 +162,8 @@ class TestSQSClientBase:
         spy_info.assert_called_once_with(
             caplog.records[0].message,
             extra={
-                "attributes": message.metadata,
-                "body": message.body,
+                "attributes": sample_broker_message.metadata,
+                "body": sample_broker_message.body,
             },
         )
 
@@ -178,7 +194,7 @@ class TestSQSClient:
     def test_uses_sqs_client_meta_metaclass(self) -> None:
         assert isinstance(SQSClient, SQSClientMeta)
 
-    def test_connect_initializes_sqs_client_on_first_call(
+    def test_connect_initializes_client_on_first_call(
         self, sample_sqs_client: SQSClient, mock_boto3_client: MagicMock
     ) -> None:
         sample_sqs_client.connect()
@@ -186,7 +202,7 @@ class TestSQSClient:
         assert sample_sqs_client._client is not None
         mock_boto3_client.assert_called_once_with("sqs", region_name="region_name", endpoint_url="endpoint_url")
 
-    def test_connect_does_not_reinitialize_sqs_client_on_second_call(
+    def test_connect_does_not_reinitialize_client_on_second_call(
         self, sample_sqs_client: SQSClient, mock_boto3_client: MagicMock
     ) -> None:
         sample_sqs_client.connect()
@@ -195,7 +211,7 @@ class TestSQSClient:
         assert sample_sqs_client._client is not None
         mock_boto3_client.assert_called_once_with("sqs", region_name="region_name", endpoint_url="endpoint_url")
 
-    def test_disconnect_closes_sqs_client_when_client_is_open(
+    def test_disconnect_closes_client_when_client_is_open(
         self, sample_sqs_client: SQSClient, mock_boto3_client: MagicMock
     ) -> None:
         sample_sqs_client.connect()
@@ -203,11 +219,61 @@ class TestSQSClient:
 
         mock_boto3_client.return_value.close.assert_called_once()
 
-    def test_disconnect_does_not_close_sqs_client_when_client_is_not_open(
+    def test_disconnect_does_not_close_client_when_client_is_not_open(
         self, sample_sqs_client: SQSClient, mock_boto3_client: MagicMock
     ) -> None:
         sample_sqs_client.disconnect()
         mock_boto3_client.return_value.close.assert_not_called()
+
+    def test_send_message_success_returns_response(
+        self,
+        mocker: MockerFixture,
+        sample_sqs_client: SQSClient,
+        mock_boto3_client: MagicMock,
+        sample_broker_message: BrokerMessage,
+    ) -> None:
+        spy_log_success = mocker.spy(sample_sqs_client, "log_success")
+        mock_boto3_client.return_value.send_message.return_value = "response"
+
+        sample_sqs_client.connect()
+        result = sample_sqs_client.send_message(sample_broker_message)
+
+        assert result == "response"
+        spy_log_success.assert_called_once_with(sample_broker_message)
+
+    def test_send_message_client_error_logs_error(
+        self,
+        mocker: MockerFixture,
+        sample_sqs_client: SQSClient,
+        mock_boto3_client: MagicMock,
+        sample_broker_message: BrokerMessage,
+        sample_client_error: ClientError,
+    ) -> None:
+        spy_log_client_error = mocker.spy(sample_sqs_client, "log_client_error")
+        mock_boto3_client.return_value.send_message.side_effect = sample_client_error
+
+        sample_sqs_client.connect()
+        result = sample_sqs_client.send_message(sample_broker_message)
+
+        assert result is None
+        spy_log_client_error.assert_called_once_with(sample_client_error)
+
+    def test_send_message_boto_core_error_logs_error(
+        self,
+        mocker: MockerFixture,
+        sample_sqs_client: SQSClient,
+        mock_boto3_client: MagicMock,
+        sample_broker_message: BrokerMessage,
+        sample_boto_core_error: BotoCoreError,
+    ) -> None:
+        spy_log_boto_core_error = mocker.spy(sample_sqs_client, "log_boto_core_error")
+        mock_boto3_client.return_value.send_message.side_effect = sample_boto_core_error
+
+        sample_sqs_client.connect()
+        result = sample_sqs_client.send_message(sample_broker_message)
+
+        assert result is None
+        spy_log_boto_core_error.assert_called_once_with(sample_boto_core_error)
 
 
 class TestAsyncSQSClient:
@@ -229,7 +295,7 @@ class TestAsyncSQSClient:
         assert isinstance(AsyncSQSClient, SQSClientMeta)
 
     @pytest.mark.asyncio
-    async def test_connect_initializes_async_sqs_client_on_first_call(
+    async def test_connect_initializes_client_on_first_call(
         self, sample_async_sqs_client: AsyncSQSClient, mock_aioboto3_session: MagicMock
     ) -> None:
         await sample_async_sqs_client.connect()
@@ -240,7 +306,7 @@ class TestAsyncSQSClient:
         )
 
     @pytest.mark.asyncio
-    async def test_connect_does_not_reinitialize_async_sqs_client_on_second_call(
+    async def test_connect_does_not_reinitialize_client_on_second_call(
         self, sample_async_sqs_client: AsyncSQSClient, mock_aioboto3_session: MagicMock
     ) -> None:
         await sample_async_sqs_client.connect()
@@ -252,7 +318,7 @@ class TestAsyncSQSClient:
         )
 
     @pytest.mark.asyncio
-    async def test_disconnect_closes_async_sqs_client_when_client_is_open(
+    async def test_disconnect_closes_client_when_client_is_open(
         self, sample_async_sqs_client: AsyncSQSClient, mock_aioboto3_session: MagicMock
     ) -> None:
         await sample_async_sqs_client.connect()
@@ -262,8 +328,67 @@ class TestAsyncSQSClient:
         assert "__aexit__" in mock_aioboto3_session.return_value.client.return_value.mock_calls[1][0]
 
     @pytest.mark.asyncio
-    async def test_disconnect_does_not_close_async_sqs_client_when_client_is_not_open(
+    async def test_disconnect_does_not_close_client_when_client_is_not_open(
         self, sample_async_sqs_client: AsyncSQSClient, mock_aioboto3_session: MagicMock
     ) -> None:
         await sample_async_sqs_client.disconnect()
         assert len(mock_aioboto3_session.return_value.client.return_value.mock_calls) == 0
+
+    @pytest.mark.asyncio
+    async def test_send_message_success_returns_response(
+        self,
+        mocker: MockerFixture,
+        sample_async_sqs_client: AsyncSQSClient,
+        mock_aioboto3_session: MagicMock,
+        sample_broker_message: BrokerMessage,
+    ) -> None:
+        spy_log_success = mocker.spy(sample_async_sqs_client, "log_success")
+        mock_aioboto3_session.return_value.client.return_value.__aenter__.return_value.send_message.return_value = (
+            "response"
+        )
+
+        await sample_async_sqs_client.connect()
+        result = await sample_async_sqs_client.send_message(sample_broker_message)
+
+        assert result == "response"
+        spy_log_success.assert_called_once_with(sample_broker_message)
+
+    @pytest.mark.asyncio
+    async def test_send_message_client_error_logs_error(
+        self,
+        mocker: MockerFixture,
+        sample_async_sqs_client: AsyncSQSClient,
+        mock_aioboto3_session: MagicMock,
+        sample_broker_message: BrokerMessage,
+        sample_client_error: ClientError,
+    ) -> None:
+        spy_log_client_error = mocker.spy(sample_async_sqs_client, "log_client_error")
+        mock_aioboto3_session.return_value.client.return_value.__aenter__.return_value.send_message.side_effect = (
+            sample_client_error
+        )
+
+        await sample_async_sqs_client.connect()
+        result = await sample_async_sqs_client.send_message(sample_broker_message)
+
+        assert result is None
+        spy_log_client_error.assert_called_once_with(sample_client_error)
+
+    @pytest.mark.asyncio
+    async def test_send_message_boto_core_error_logs_error(
+        self,
+        mocker: MockerFixture,
+        sample_async_sqs_client: AsyncSQSClient,
+        mock_aioboto3_session: MagicMock,
+        sample_broker_message: BrokerMessage,
+        sample_boto_core_error: BotoCoreError,
+    ) -> None:
+        spy_log_boto_core_error = mocker.spy(sample_async_sqs_client, "log_boto_core_error")
+        mock_aioboto3_session.return_value.client.return_value.__aenter__.return_value.send_message.side_effect = (
+            sample_boto_core_error
+        )
+
+        await sample_async_sqs_client.connect()
+        result = await sample_async_sqs_client.send_message(sample_broker_message)
+
+        assert result is None
+        spy_log_boto_core_error.assert_called_once_with(sample_boto_core_error)
